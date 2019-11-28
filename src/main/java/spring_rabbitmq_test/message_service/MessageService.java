d221dfe86f92275131e2207bad3b9a9f10f5dd05
ken <kenyujy@gmail.com>
2019-11-28 17:19:42 +0800

    update listener service

--- src/main/java/spring_rabbitmq_test/message_service/MessageService.java
+++ src/main/java/spring_rabbitmq_test/message_service/MessageService.java
@@ -1,16 +1,23 @@
 package spring_rabbitmq_test.message_service;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import spring_rabbitmq_test.entity.Msg;
 
+import java.io.IOException;
+
 @Service
 public class MessageService {
 
     @RabbitListener(queues ="test")  //listener 会一直监听消息队列
-    public void getTestMessage(Object message, Msg msg){  //收消息, 自动转换为Msg 对象类型
-        System.out.println( message);
+    public void getTestMessage(Message message, Msg msg, Channel channel) throws IOException {  //收消息, 自动转换为Msg 对象类型
+
+        System.out.println(message);
+        //channel.basicReject(message.getMessageProperties().getDeliveryTag(), true); //requeue =true
     }
 }
 
